0151f8b50a654571c4a6337af82bcab382aef74b
Josué Dueñas <josueduenas2805@gmail.com>
2023-09-07 21:42:20 -0600

    Agregando editor de apuntes y funcionalidad

---
+++ frontend/app/src/views/ComponenteEditor.vue
@@ -0,0 +1,33 @@
+<template>
+    <ion-page>
+        <ion-header>
+            <ion-toolbar>
+                <ion-buttons slot="start">
+                    <ion-back-button></ion-back-button>
+                </ion-buttons>
+
+                <ion-title>Editor</ion-title>
+            </ion-toolbar>
+        </ion-header>
+
+        <ion-content class="ion-padding">
+            <EditorApuntes />
+        </ion-content>
+    </ion-page>
+</template>
+
+<script>
+import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent, IonBackButton, IonButtons } from '@ionic/vue'
+import EditorApuntes from '../views/EditorApuntes.vue'
+
+export default {
+    name: 'EditorPage',
+    components: {
+        IonPage, IonHeader, IonToolbar, IonTitle, IonContent, IonBackButton, IonButtons, EditorApuntes
+    }
+}
+</script>
+
+<style>
+    
+</style>
