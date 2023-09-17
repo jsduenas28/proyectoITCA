3c6b2c948e06794a0c398fd71666d9648302d831
Josué Dueñas <josueduenas2805@gmail.com>
2023-09-17 10:15:04 -0600

    agregando rutas URI para carpetas y validaciones

---
+++ backend/api/app/Http/Controllers/Api/CarpetaController.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+use App\Models\Carpeta;
+
+class CarpetaController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $user = auth()->user();
+        $carpeta = Carpeta::where('usuario', $user->id)->get();
+        return $carpeta;
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $user = auth()->user();
+        $carpeta = new Carpeta();
+        $carpeta->nombre_carpeta = $request->nombre_carpeta;
+        $carpeta->color_carpeta = $request->color_carpeta;
+        $carpeta->usuario = $user->id;
+
+        $carpeta->save();
+
+        return response()->json(['message' => 'Carpeta creada correctamente', 'data' => $carpeta]);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit($id)
+    {
+        $carpeta = Carpeta::where('id', $id)->get();
+
+        return $carpeta;
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request)
+    {
+        $carpeta = Carpeta::findOrFail($request->id);
+
+        $carpeta->nombre_carpeta = $request->nombre_carpeta;
+        $carpeta->color_carpeta = $request->color_carpeta;
+        $carpeta->usuario = $request->usuario;
+
+        $carpeta->save();
+
+        // Respuesta JSON con los datos actualizados
+        return response()->json(['message' => 'Carpeta actualizada correctamente', 'data' => $carpeta]);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy($id)
+    {
+        $carpeta = Carpeta::where('id', $id)->delete();
+        return response()->json(['message' => 'Carpeta eliminada correctamente', 'data' => $carpeta]);
+    }
+}
---
+++ backend/api/app/Models/Carpeta.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Carpeta extends Model
+{
+    protected $table = 'carpeta';
+    protected $primaryKey = 'id';
+    protected $fillable = ['nombre_carpeta', 'color_carpeta', 'usuario'];
+}
