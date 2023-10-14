3bc0200d3cf8aa1909a5fe1575382b0bca11e5a7
Josué Dueñas <josueduenas2805@gmail.com>
2023-10-14 11:27:23 -0600

    agregue toda la funcionalidad para cuestionarios
    La generación de cuestionarios ya funciona bien y sin problemas

--- backend/api/app/Http/Controllers/Api/GPTController.php
+++ backend/api/app/Http/Controllers/Api/GPTController.php
@@ -2,43 +2,32 @@
 
 namespace App\Http\Controllers\Api;
 
-use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
-use OpenAI\Laravel\Facades\OpenAI;
+use Exception;
 use App\Models\Nota;
+use GuzzleHttp\Client;
+use App\Http\Controllers\Controller;
 
 class GPTController extends Controller
 {
-    function genCuestionario ($id) {
+    function genCuestionario($id) {
+        $nota = Nota::where('id', $id)->value('contenido_nota');
 
-        $nota = Nota::where('id', $id)->get();
-
-        $texto = $nota[0]['contenido_nota'];
-
-        $result = OpenAI::completions()->create([
-            'model' => 'text-davinci-003',
-            'temperature' => 1,
-            'top_p' => 1,
-            'max_tokens' => 2000,
-            'prompt' => '
-                Genera una pregunta usando un texto como contexto para la pregunta, genera tambien las opciones
-                para responder la pregunta, usa el siguiente formato:
-
-                {
-                    "pregunta": "string",
-                    "respuestas": "array" ["string"]
-                    "resCorrecta": "string"
-                }
-
-                agrega la información que generes dentro del formato JSON y retornalo, NO RETORNES NADA
-                QUE NO VAYA A ESTAR DENTRO DEL OBJETO JSON POR FAVOR, no quiero ningun otro tipo de texto
-                fuera del objeto JSON, agrega las respuestas en "respuestas"
-                y separa la respuesta correcta en "resCorrecta", NO PUEDEN HABER COMAS EN "resCorrecta".
-
-                Texto:
-            '.$texto
+        $client = new Client();
+        $response = $client->post('https://api.opexams.com/questions-generator', [
+            'headers' => [
+                'Content-Type' => 'application/json',
+                'api-key' => 'sk20YuH47nKzolYdmxdHDz30CAv58eD'
+            ],
+            'json' => [
+                "type" => "contextBased",
+                "context" => $nota,
+                "questionType" => "MCQ",
+                "language"=> "spanish"
+            ]
         ]);
 
-        return $result['choices'][0]['text'];
+        $body = $response->getBody();
+
+        return response()->json(json_decode($body, true));
     }
 }
--- frontend/app/src/views/ComponenteExamenCreado.vue
+++ frontend/app/src/views/ComponenteExamenCreado.vue
@@ -53,38 +53,7 @@ export default {
     },
     data() {
         return {
-            objectoRes:  [
-                {
-                    "id": "pDgnjdMzOt9",
-                    "question": "¿Cuál fue la mayor contienda bélica de la historia?",
-                    "answer": "Segunda Guerra Mundial",
-                    "options": ["Primera Guerra Mundial", "Guerra de Vietnam", "Guerra de los Balcanes", "Segunda Guerra Mundial", "Guerra Fría"]
-                },
-                {
-                    "id": "GZG49mXC3Jy",
-                    "question": "¿Cuántos militares se movilizaron en la Segunda Guerra Mundial?",
-                    "answer": "100 millones",
-                    "options": ["100 millones", "50 millones", "70 millones", "80 millones", "120 millones"]
-                },
-                {
-                    "id": "r0SclR9bOYf",
-                    "question": "¿Cuáles fueron las dos alianzas militares enfrentadas en la Segunda Guerra Mundial?",
-                    "answer": "Aliados y Potencias del Eje",
-                    "options": ["Aliados y Comunistas", "Alemania y Francia", "Unión Soviética y Japón", "Aliados y Potencias del Eje", "Estados Unidos y Rusia"]
-                },
-                {
-                    "id": "Tjadx7EYWKL",
-                    "question": "¿Qué reivindicación expansionista llevó a Hitler a invadir Polonia?",
-                    "answer": "El Corredor Polaco",
-                    "options": ["Anexión de Alsacia-Lorena", "El Corredor Polaco", "Anexión de los Sudetes", "Anexión de Austria", "Anexión de Eslovaquia"]
-                },
-                {
-                    "id": "Z9TxfwpFzvN",
-                    "question": "¿Qué técnica militar desarrolló Alemania en la Segunda Guerra Mundial?",
-                    "answer": "Blitzkrieg",
-                    "options": ["Guerra de trincheras", "Guerra de mentira", "Guerra de guerrillas", "Guerra psicológica", "Blitzkrieg"]
-                }
-            ],
+            objectoRes:  [],
             objetoPregunta: {},
             indicePreguntaActual: 0,
             resolverPregunta: false,
@@ -123,37 +92,34 @@ export default {
                 throw error; // Manejo de errores, si es necesario
             }
         },
-        obtenerCuestionario() {
-            const peticion = {
-                "type": "contextBased",
-                "context": "La Segunda Guerra Mundial (también escrito II Guerra Mundial)1​ fue un conflicto militar global que se desarrolló entre 1939 y 1945. En ella se vieron implicadas la mayor parte de las naciones del mundo —incluidas todas las grandes potencias, así como prácticamente todas las naciones europeas— agrupadas en dos alianzas militares enfrentadas: los Aliados, por un lado, y las Potencias del Eje, por otro. Fue la mayor contienda bélica de la historia, con más de 100 millones de militares movilizados y un estado de guerra total en que los grandes contendientes destinaron toda su capacidad económica, militar y científica al servicio del esfuerzo bélico, borrando la distinción entre recursos civiles y militares. Marcada por hechos de enorme repercusión que incluyeron la muerte masiva de civiles (el Holocausto, los bombardeos masivos sobre ciudades y el uso, por primera vez en un conflicto militar, de armas nucleares), la Segunda Guerra Mundial fue la más mortífera de la historia, con un resultado de entre 50 y 70 millones de víctimas, el 2,5 % de la población mundial.2​ ",
-                "questionType": "MCQ",
-                "language": "spanish"
+        async obtenerCuestionario() {
+            try {
+                await this.obtenerToken()
+                var nota = this.$route.params.nota
+
+                axios.get(`/api/genCuestionario/${nota}`, {
+                    headers: {
+                        'Authorization': 'Bearer ' + this.token
+                    }
+                })
+                .then(response => {
+                    this.objectoRes = response.data
+                    this.generarCuestionario()
+                })
+                .catch(error => console.error(error))
+            } catch (error) {
+                console.error(error);
             }
-
-            var apiKey = "sk20YuH47nKzolYdmxdHDz30CAv58eD"
-
-            axios.post('https://api.opexams.com/questions-generator', JSON.stringify(peticion), {
-                headers: {
-                    "Access-Control-Allow-Headers": "Content-Type",
-                    "api-key": apiKey,
-                    "request-type": "test",
-                    "Content-Type": "application/json"
-                }
-            })
-            .then(response => {
-                console.log(respose.data)
-            })
-            .catch(error => console.error(error))
         },
         generarCuestionario() {
-            if(this.indicePreguntaActual < this.objectoRes.length) {
+            if(this.indicePreguntaActual < this.objectoRes.data.length) {
                 this.respuestaSelect = null
                 this.resolverPregunta = false
-                this.objetoPregunta = this.objectoRes[this.indicePreguntaActual]
+                this.objetoPregunta = this.objectoRes.data[this.indicePreguntaActual]
                 this.indicePreguntaActual++
-            } else {
-                alert('Respondidas: ' + this.respuestasCorrectas + '/' + this.objectoRes.length)
+            } 
+            else {
+                alert('Respondidas: ' + this.respuestasCorrectas + '/' + this.objectoRes.data.length)
             }
         },
         validarPregunta() {
@@ -171,8 +137,7 @@ export default {
         }
     },
     mounted() {
-        //this.obtenerCuestionario()
-        this.generarCuestionario()
+        this.obtenerCuestionario()
         this.getNota()
     },
 }
